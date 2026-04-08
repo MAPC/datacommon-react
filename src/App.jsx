b3be2a9ffb32c7a320eec1451a6ab37b138f9fac
ztocode <miraclezag@gmail.com>
2026-04-08 08:32:51 -0400

    Revert "display welcome modal"
    This reverts commit 52c557c16f5e4bd21beff1b4bd03f782c93550bb.
    
    revert commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import IntroModal from './components/IntroModal'
 
 function App() {
   return (
@@ -12,7 +11,7 @@ function App() {
         <Outlet />
       </main>
       <Footer />
-      <IntroModal />
+      {/* <IntroModal /> */}
     </section>
   )
 }
