e880a03e518e95f121abcef7ee025ea5d9831da8
ztocode <miraclezag@gmail.com>
2026-04-17 15:18:39 -0400

    Refine the download functionality on the dataset page

--- src/components/partials/DatasetHeader.jsx
+++ src/components/partials/DatasetHeader.jsx
@@ -1,151 +1,7 @@
-import React, { useState, useRef, useEffect, useMemo } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import PropTypes from "prop-types";
 import { formatUpdated } from "../../utils/formatUpdated";
-
-const formats = {
-  csv: {
-    extension: ".csv",
-    isGeospatial: true,
-    isTabular: true,
-    zoningAtlas: "https://mapc365.sharepoint.com/:x:/s/DataServicesSP/Efonrnmw_kdMhmG3Dw2BkTcBIpe2sC_2ADWTWfUjOs4JhQ?e=K65BCE",
-    displayName: "CSV",
-  },
-  json: {
-    extension: ".json",
-    isGeospatial: false,
-    isTabular: true,
-    zoningAtlas: "",
-    displayName: "JSON",
-  },
-  shapefile: {
-    extension: ".shp",
-    isGeospatial: true,
-    isTabular: false,
-    zoningAtlas: "https://mapc365.sharepoint.com/:f:/s/DataServicesSP/ErKkXSLH_iBOlDhJrTXldrYBIIZ4ZXe4Bkw7OyVapVpX3Q?e=iRkWVB",
-    displayName: "ESRI Shapefile",
-  },
-  geojson: {
-    extension: ".geojson",
-    isGeospatial: true,
-    isTabular: false,
-    zoningAtlas: "",
-    displayName: "GeoJSON",
-  },
-};
-
-/**
- * Downloads metadata in CSV format
- * @param {Event} e - The event object
- * @param {string} database - The database name
- * @param {Object} metadata - The metadata object
- * @param {string} title - The title of the dataset
- * @param {string} table - The table name
- * @param {string} description - The description of the dataset
- */
-const downloadMetadata = (e, database, metadata, title, table = "", description = "") => {
-  // TODO: Make this a cached download from the server as well
-  e.preventDefault();
-  const documentHeader = ["name", "alias", "details"];
-  let rows;
-  if (database === "towndata" || database === "gisdata") {
-    const metadataName = metadata.documentation.metadata.eainfo.detailed.attr.map((attr) => (attr.attrlabl ? attr.attrlabl : "undefined"));
-    const metadataAlias = metadata.documentation.metadata.eainfo.detailed.attr.map((attr) => attr.attalias);
-    const metadataDescription = metadata.documentation.metadata.eainfo.detailed.attr.map((attr) => (attr.attrdef ? attr.attrdef : "undefined"));
-    rows = [
-      ["title", "Title", title],
-      ["tbl_table", "Table", table],
-      ["descriptn", "Description", description],
-    ].concat(metadataName.map((item, i) => [item, metadataAlias[i], metadataDescription[i]]));
-  } else {
-    const values = metadata.map((row) => documentHeader.map((key) => row[key]));
-    rows = values.map((row) => row.reduce((a, b) => `${a},${b}`));
-  }
-  const csvHeader = "data:text/csv;charset=utf-8,";
-  const documentRows = rows.reduce((a, b) => `${a}\n${b}`);
-
-  const documentStructure = [[documentHeader], documentRows].reduce((a, b) => a.concat(b));
-  const documentBody = documentStructure.reduce((a, b) => `${a}\n${b}`);
-
-  const csvFile = csvHeader + documentBody;
-  const encoded = encodeURI(csvFile);
-  const fileName = `${title}-metadata.csv`;
-
-  const link = document.createElement("a");
-  link.setAttribute("href", encoded);
-  link.setAttribute("download", fileName);
-
-  document.body.appendChild(link);
-  link.click();
-};
-
-const urlForDownload = (
-  schema, table, database, selectedYears, queryYearColumn, selectedColumns, columnKeys, selectedGeographies, availableGeographies, geographyColumn, filterExportData, format
-) => {
-  let url = "#";
-
-  // Handle zoning atlas special case
-  if (table === "zoning_atlas") {
-    return formats[format].zoningAtlas || "#";
-  }
-
-  // Build query and fetch data based on whether years are selected
-  url = `/api/export?token=${import.meta.env.VITE_MAPC_API_TOKEN}&database=${database}&schema=${schema}&table=${table}&format=${format}`;
-  // TODO: Should the filter export checkbox apply to years too?
-  if (selectedYears.length > 0 && queryYearColumn !== "") {
-    url = `${url}&years=${selectedYears.join(",")}`;
-  }
-
-  // Include all columns by default if the users hasn't de-selected any
-  // Respect the checkbox for if data should be filtered
-  if (selectedColumns.length && selectedColumns.length !== columnKeys.length && filterExportData) {
-    url = `${url}&columns=${selectedColumns.join(',')}`
-  }
-
-  // Include all geographies by default if the users hasn't de-selected any
-  // Respect the checkbox for if data should be filtered
-  if (selectedGeographies.length && selectedGeographies.length !== availableGeographies.length && geographyColumn && filterExportData) {
-    // Some geographies have the "&" character, need to be encoded
-    const mappedGeos = selectedGeographies.map(col => encodeURIComponent(col));
-    url = `${url}&geographies=${mappedGeos.join(',')}&geoColumn=${geographyColumn}`
-  }
-
-  return url;
-};
-
-const setDownloadButton = (
-  metadata, schema, table, title, description, selectedYears, queryYearColumn, selectedColumns, columnKeys, selectedGeographies, availableGeographies, geographyColumn, filterExportData, database
-) => {
-  const tableIsGeospatial = database === "towndata" || database === "gisdata";
-  return (
-    <div className="details-content-column download-links">
-      Download:
-      <div className="download-buttons">
-        <div className="button file-button" onClick={(e) => downloadMetadata(e, database, metadata, title, table, description)}>
-          .metadata
-        </div>
-        {Object.entries(formats)
-          .filter(([format, config]) => config.isGeospatial === tableIsGeospatial || (!tableIsGeospatial && config.isTabular)) // eslint-disable-line no-unused-vars
-          .map(([format, config]) => (
-            <a
-              key={format}
-              target="_blank"
-              rel="noopener noreferrer"
-              title={`Download data as ${config.displayName}`}
-              download
-              className="button file-button"
-              href={
-                urlForDownload(
-                  schema, table, database, selectedYears, queryYearColumn, selectedColumns, columnKeys, selectedGeographies, availableGeographies, geographyColumn, filterExportData, format
-                )
-              }
-            >
-              {config.extension}
-            </a>
-          ))}
-      </div>
-    </div>
-  );
-};
+import ExportDataModal from "./ExportDataModal";
 
 const setSelectYears = (availableYears, updateSelectedYears, selectedYears) => {
   if (availableYears.length > 0) {
@@ -569,11 +425,7 @@ function DatasetHeader({
   updateSelectedGeographies,
   geographyColumn,
 }) {
-  const [filterExportData, setFilterExportData] = useState(true);
-
-  const showFilterExportCheckbox = useMemo(() => {
-    return columnKeys.length !== selectedColumns.length || availableGeographies.length !== selectedGeographies.length;
-  }, [columnKeys, selectedColumns, selectedColumns.length, availableGeographies, selectedGeographies, selectedGeographies.length]);
+  const [downloadModalOpen, setDownloadModalOpen] = useState(false);
 
   return (
     <div className="page-header">
@@ -612,34 +464,11 @@ function DatasetHeader({
             </div>
           </div>
           <div className="details-content-column download-section">
-            {setDownloadButton(
-              metadata, schema, table, title, description, selectedYears, queryYearColumn, selectedColumns, columnKeys, selectedGeographies, availableGeographies, geographyColumn, filterExportData, database
-            )}
-            {showFilterExportCheckbox && <div className="download-should-filter-checkbox-container">
-              <label title="Should the exported data be filtered using the current selections?">
-                Export filtered data only
-              </label>
-              <input
-                className="download-should-filter-checkbox"
-                type="checkbox"
-                checked={filterExportData}
-                onChange={(e) => {
-                  e.stopPropagation();
-                  setFilterExportData(e.target.checked);
-                }}
-              />
-            </div>}
-            {!!datasetId && (
-              <div style={{ marginTop: "10px", textAlign: "right" }}>
-                <a
-                  href={`/developers?datasetId=${encodeURIComponent(String(datasetId))}`}
-                  className="button feedback-button"
-                  style={{ fontSize: "12px" }}
-                >
-                  Generate API
-                </a>
-              </div>
-            )}
+            <div className="details-content-column download-links">
+              <button type="button" className="button file-button" onClick={() => setDownloadModalOpen(true)}>
+                Export data
+              </button>
+            </div>
             <div style={{ marginTop: "10px", textAlign: "right" }}>
               <a
                 href="https://airtable.com/appqSr3MqAkN1GCfb/pagdcSeY2bc4rblam/form"
@@ -654,6 +483,25 @@ function DatasetHeader({
           </div>
         </div>
       </div>
+      <ExportDataModal
+        isOpen={downloadModalOpen}
+        onClose={() => setDownloadModalOpen(false)}
+        datasetId={datasetId}
+        title={title}
+        table={table}
+        description={description}
+        database={database}
+        schema={schema}
+        metadata={metadata}
+        columnKeys={columnKeys}
+        selectedColumns={selectedColumns}
+        selectedYears={selectedYears}
+        queryYearColumn={queryYearColumn}
+        selectedGeographies={selectedGeographies}
+        availableGeographies={availableGeographies}
+        geographyColumn={geographyColumn}
+        availableYears={availableYears}
+      />
     </div>
   );
 }
