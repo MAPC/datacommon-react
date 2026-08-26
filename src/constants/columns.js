62e19d64fa5b74146edbd79472c0d904b40f3b9e
Brooke Melody <bmelody@mapc.org>
2026-08-26 15:35:27 -0400

    adds more columns that shouldn't be formated

--- src/constants/columns.js
+++ src/constants/columns.js
@@ -1,10 +1,12 @@
 export const unformattedColumns = [
   // year columns:
-  "fy_year", "fiscal_yr", "fy", "acs_year", "year", "dec_year", "cal_year",
+  "fy_year", "fiscal_yr", "fy", "acs_year", "year", "dec_year", "cal_year", "schoolyear",
   // census tract columns:
   "ct10_id", "ct20_id", "geoid", "logrecno", 
   // block group columns:
   "bg10_id", "bg20_id", 
   // blocks columns:
   "blk10_id", "blk20_id",
+  // other specific columns:
+  "schid",
 ]
