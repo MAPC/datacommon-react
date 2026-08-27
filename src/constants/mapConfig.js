c0dd7c10a39f6ece2c549050b0543a1d279eef3f
Brooke Melody <bmelody@mapc.org>
2026-08-27 09:46:35 -0400

    Small UI fixes

--- src/constants/mapConfig.js
+++ src/constants/mapConfig.js
@@ -1,10 +1,13 @@
 export const MAP_CONFIG = {
-  accessToken: "pk.eyJ1IjoiaWhpbGwiLCJhIjoiY2plZzUwMTRzMW45NjJxb2R2Z2thOWF1YiJ9.szIAeMS4c9YTgNsJeG36gg",
   style: "mapbox://styles/ihill/ckeucj9gy9vt319qm4dxcn73l",
+  // bounds are used for the community profiles muni picker view
   bounds: [
     [-74.0081481933594, 41.1863288879395],
     [-69.8615341186523, 42.8867149353027],
   ],
+  // center and zoom are used for the maps within a community profile
+  center: [-71.5, 42.08],
+  zoom: 6.9,
   padding: { top: 30, left: 300, right: 30, bottom: 30 },
   navigationControl: {
     showCompass: false,
