43d9b9e0f6bfd76b0d3286204c3294b7775bb0af
Brooke Melody <bmelody@mapc.org>
2026-08-28 11:38:17 -0400

    Fixes navigating to munis with spaces in map links

--- src/components/visualizations/MunicipalFinanceOverridesMap.jsx
+++ src/components/visualizations/MunicipalFinanceOverridesMap.jsx
@@ -237,7 +237,7 @@ export default function MunicipalFinanceOverridesMap({ config, municipalFeature 
     : "";
 
   const selectedProfileUrl = selectedDisplayName
-    ? `/profile/${muniProfileSlug(selectedDisplayName)}/municipal-finance`
+    ? `/profile/${muniProfileSlug(selectedDisplayName.toLowerCase().replace(/\s+/g, '-'))}/municipal-finance`
     : null;
 
   const latestDataRef = useRef({
